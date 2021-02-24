f96b6534cd2d1d71006bc9e65e8a1d96811864c2
Milo Silva <milo.silva234@gmail.com>
2021-02-24 14:07:45 +0100

    comment out use-gesture

--- SpringDemo.js
+++ SpringDemo.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { useSpring, animated, useSprings, interpolate } from "react-spring";
-import { useGesture } from "react-use-gesture";
+// import { useGesture } from "react-use-gesture";
 // React-native
 const AnimatedView = animated(View);
 const cards = [
@@ -23,40 +23,41 @@ export default function SpringDemo() {
   const [gone] = useState(() => new Set()); // The set flags all the cards that are flicked out
   const [props, set] = useSprings(cards.length, (i) => ({ ...to(i), from: from(i) })); // Create a bunch of springs using the helpers above
   // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
-  const bind = useGesture(
-    ({ args: [index], down, delta: [xDelta], distance, direction: [xDir], velocity }) => {
-      const trigger = velocity > 0.2; // If you flick hard enough it should trigger the card to fly out
-      const dir = xDir < 0 ? -1 : 1; // Direction should either point left or right
-      if (!down && trigger) gone.add(index); // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
-      set((i) => {
-        if (index !== i) return; // We're only interested in changing spring-data for the current spring
-        const isGone = gone.has(index);
-        const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
-        const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0); // How much the card tilts, flicking it harder makes it rotate faster
-        const scale = down ? 1.1 : 1; // Active cards lift up a bit
-        return {
-          x,
-          rot,
-          scale,
-          delay: undefined,
-          config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 },
-        };
-      });
-      if (!down && gone.size === cards.length)
-        setTimeout(() => gone.clear() || set((i) => to(i)), 600);
-    }
-  );
+  // const bind = useGesture(
+  //   ({ args: [index], down, delta: [xDelta], distance, direction: [xDir], velocity }) => {
+  //     const trigger = velocity > 0.2; // If you flick hard enough it should trigger the card to fly out
+  //     const dir = xDir < 0 ? -1 : 1; // Direction should either point left or right
+  //     if (!down && trigger) gone.add(index); // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
+  //     set((i) => {
+  //       if (index !== i) return; // We're only interested in changing spring-data for the current spring
+  //       const isGone = gone.has(index);
+  //       const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0; // When a card is gone it flys out left or right, otherwise goes back to zero
+  //       const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0); // How much the card tilts, flicking it harder makes it rotate faster
+  //       const scale = down ? 1.1 : 1; // Active cards lift up a bit
+  //       return {
+  //         x,
+  //         rot,
+  //         scale,
+  //         delay: undefined,
+  //         config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 },
+  //       };
+  //     });
+  //     if (!down && gone.size === cards.length)
+  //       setTimeout(() => gone.clear() || set((i) => to(i)), 600);
+  //   }
+  // );
   // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
   return props.map(({ x, y, rot, scale }, i) => (
     <AnimatedView
       key={i}
       style={{ transform: interpolate([x, y], (x, y) => [{ translateX: x }, { translateY: y }]) }}
     >
+      <Image source={{ uri: cards[i] }} /* {...bind(i)} */ style={{ width: 150, height: 264 }} />
       {/* This is the card itself, we're binding our gesture to it (and inject its index so we know which is which) */}
-      <AnimatedView
+      {/* <Image
         {...bind(i)}
         style={{ transform: interpolate([rot, scale], trans), backgroundImage: `url(${cards[i]})` }}
-      />
+      /> */}
     </AnimatedView>
   ));
 }
